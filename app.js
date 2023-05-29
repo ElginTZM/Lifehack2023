09c0c5c13484afc9c514c664dc6bd803842741b7
fuyiqiao <yiqiao_fu@hotmail.com>
2023-05-29 22:04:24 +0800

    home page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 console.log("start");
 
 app.get('/', (req, res) => {
-    res.sendFile('./templates/index.html', {root: __dirname});
+    res.sendFile('./templates/home.html', {root: __dirname});
 });
 
 const server = app.listen(3000);
