353583034a93796d3a5a0ff312b18e5850b5181b
Elgin Tan <ElginTZM@u.nus.edu>
2023-05-29 18:42:25 +0800

    Add endpoint for parsing pdf file

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express")
+const fileUpload = require("express-fileupload")
+const PDFParser = import("pdf2json")
 
 const app = express();
+app.use(fileUpload());
 
-console.log("start");
+Promise.all([PDFParser]).then(([PDFParser]) => {
+    app.post('/parsePDF', (req, res) => {
+        if (!req.files) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        let file = req.files.uploadFile;
+        let uploadPath = __dirname + '/uploads/' + file.name;
+        let regex = /(----------------Page \(\w\) Break----------------)/g;
+
+        const pdfParser = new PDFParser.default(this, 1);
+        pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+        pdfParser.on("pdfParser_dataReady", pdfData => {
+            res.send(pdfParser.getRawTextContent().replace(regex, ""));
+        });
+    
+        pdfParser.parseBuffer(file.data);
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile('./templates/index.html', {root: __dirname});
